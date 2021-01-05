5cd517bcf425bfb7004de2d8e63dccfcbedcc5cc
Boyul Kim <boyulkim1@gmail.com>
2021-01-04 17:01:44 -0800

    styled page using boostrap and css

--- client/pages/task.jsx
+++ client/pages/task.jsx
@@ -20,65 +20,62 @@ export default class Task extends React.Component {
       <div className="container-fluid mt-3">
         <h2>View Task</h2>
 
-        <div>
-          {
-          this.state.task.map(task => {
-            return (
-              <div key={task.taskId}>
-                <h6>Task Name</h6>
-                <div className="viewTask">
+        {
+        this.state.task.map(task => {
+          return (
+            <div key={task.taskId} className="mt-3">
+              <h6>Task Name</h6>
+              <div className="viewTask">
+                <div className="ml-2">
+                  {task.taskName}
+                </div>
+              </div>
+
+              <h6 className="mt-2">Status Name</h6>
+              <div className="viewTask mt-2">
+                <div className="ml-2">
+                  {task.statusName}
+                </div>
+              </div>
+
+              <div className="d-flex flex-row mt-2">
+                <h6 className="width-50 ml-2">Date Created</h6>
+                <h6 className="width-50 ml-2">Deadline</h6>
+              </div>
+
+              <div className="d-flex flex-row justify-content-around">
+                <div className="viewTask width-45">
                   <div className="ml-2">
-                    {task.taskName}
+                    {task.dateCreated}
                   </div>
                 </div>
-
-                <h6>Status Name</h6>
-                <div className="viewTask">
+                <div className="viewTask width-45">
                   <div className="ml-2">
-                    {task.statusName}
-                  </div>
-                </div>
-
-                <div className="d-flex flex-row justify-content-around">
-                  <h6>Date Created</h6>
-                  <h6>Deadline</h6>
-                </div>
-
-                <div className="d-flex flex-row justify-content-around">
-                  <div className="viewTask">
-                    <div className="ml-2">
-                      {task.dateCreated}
-                    </div>
-                  </div>
-                  <div className="viewTask">
-                    <div className="ml-2">
-                      {task.deadline}
-                    </div>
-
+                    {task.deadline}
                   </div>
 
-                </div>
-
-                <h6>Username</h6>
-                <div className="viewTask">
-                  <div className="ml-2">
-                    {task.username}
-                  </div>
-                </div>
-
-                <h6>Description</h6>
-                <div className="viewTask">
-                  <div className="ml-2">
-                    {task.description}
-                  </div>
                 </div>
 
               </div>
-            );
-          })
-          }
-        </div>
 
+              <h6 className="mt-2">Username</h6>
+              <div className="viewTask mt-2">
+                <div className="ml-2">
+                  {task.username}
+                </div>
+              </div>
+
+              <h6 className="mt-2">Description</h6>
+              <div className="viewTask height-desc mt-2">
+                <div className="ml-2">
+                  {task.description}
+                </div>
+              </div>
+
+            </div>
+          );
+        })
+        }
       </div>
     );
   }
