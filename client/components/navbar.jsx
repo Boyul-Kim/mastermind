c3072fe92328b9d86a8ae4c280d8da5699d121f5
Boyul Kim <boyulkim1@gmail.com>
2021-01-09 15:32:36 -0800

    added dropdown menu

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export default class Navbar extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-expand-xl navbar-color navbar-height">
-        <a href="#Home">
+        <a href="#home">
           <i className="fas fa-brain white fa-2x"></i>
         </a>
+        <DropdownButton id="dropdown-basic-button" title="Menu">
+          <Dropdown.Item href="#newProject">Create Project</Dropdown.Item>
+          <Dropdown.Item href="">Log out</Dropdown.Item>
+        </DropdownButton>
       </nav>
     );
   }
